03d2ccd015a69cfa66824294dfc3c42ccce67d77
Melroy <39052594+MelroyB@users.noreply.github.com>
2018-12-29 09:18:23 +0100

    Update PMKIDAttack.sh
    needed for 16800 files

--- scripts/PMKIDAttack.sh
+++ scripts/PMKIDAttack.sh
@@ -6,6 +6,7 @@ export PATH=$PATH:/sd/usr/bin:/sd/usr/sbin
 
 MYTIME=`date +%s`
 MYCMD=`cat /tmp/PMKIDAttack.run`
+MYCMD2=`cat /tmp/PMKIDAttack2.run`
 
 if [ "$1" = "start" ]; then
 	eval ${MYCMD}
@@ -14,5 +15,7 @@ if [ "$1" = "start" ]; then
 	rm -rf /tmp/PMKIDAttack.run
 elif [ "$1" = "stop" ]; then
   	killall -9 hcxdumptool
+	eval ${MYCMD2}
 	rm -rf /tmp/PMKIDAttack.run
+	rm -rf /tmp/PMKIDAttack2.run
 fi
