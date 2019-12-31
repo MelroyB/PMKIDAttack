d4eb7a36a01fd005d1c62b1f60fab4d1b9e80642
MelroyB <root@kalivm.local>
2019-12-31 13:22:24 +0100

    fix

--- api/module.php
+++ api/module.php
@@ -64,7 +64,7 @@ class PMKIDAttack extends Module
 
     protected function checkDependency($dependencyName)
     {
-        return ((exec("which {$dependencyName}") == '' ? false : true) && ($this->uciGet("PMKIDAttack.module.installed")));
+        return ($this->checkDependency($dependencyName) && ($this->uciGet("PMKIDAttack.module.installed")));
     }
 
     protected function getDevice()
@@ -80,7 +80,7 @@ class PMKIDAttack extends Module
 
     private function handleDependencies()
     {
-        if (!$this->checkDependency("hcxdumptool")) {
+        if (!$this->checkDependency("PMKIDAttack")) {
             $this->execBackground("/pineapple/modules/PMKIDAttack/scripts/dependencies.sh install ".$this->request->destination);
             $this->response = array('success' => true);
         } else {
@@ -117,7 +117,7 @@ class PMKIDAttack extends Module
     private function refreshStatus()
     {
         if (!file_exists('/tmp/PMKIDAttack.progress')) {
-            if (!$this->checkDependency("hcxdumptool")) {
+            if (!$this->checkDeps("PMKIDAttack")) {
                 $installed = false;
                 $install = "Not installed";
                 $installLabel = "danger";
